b50bb21519685523c0939af34e84906a00a63db5
Roni Seppälä <roni.o.seppala@gmail.com>
2023-10-24 12:55:56 +0300

    basic functional prototype

--- js/script.js
+++ js/script.js
@@ -2,19 +2,21 @@ let game
 
 const gameOptions = {
     windowWidth: 800,
-    windowHeight: 1000
-
+    windowHeight: 448,
+    widthOfTile: 32,
+    characterGravity: 300,
+    characterSpeed: 250
 }
 
 window.onload = function() {
     let gameConfig = {
         type: Phaser.AUTO,
-        backgroundColor: "#ADD8E6",
         scale: {
             mode: Phaser.Scale.FIT,
             autoCenter: Phaser.Scale.CENTER_BOTH,
             width: gameOptions.windowWidth,
             height: gameOptions.windowHeight,
+            margin: 0,
         },
         physics: {
             default: "arcade",
@@ -36,14 +38,58 @@ class PlayGame extends Phaser.Scene {
     }
 
     preload () {//all preloading stuffz
-        //add loading of assets here
+        this.load.image("bg","assets/stageObjects/Background/Background.png")
+        this.load.image("sideBlock","assets/stageObjects/Tiles/IndustrialTile_05.png")
+        this.load.image("innerBlock", "assets/stageObjects/Tiles/IndustrialTile_21.png")
+        this.load.spritesheet("characterIdle", "assets/character/Idle.png",{frameWidth: 72, frameHeight: 72})
     }
 
     create () {
+        this.loadLevel1()
 
+        this.character = this.physics.add.sprite(200,200,"characterIdle",0);
+        this.character.body.gravity.y = gameOptions.characterGravity;
+        this.physics.add.collider(this.character, this.groundGroup);
+
+        this.cursors = this.input.keyboard.createCursorKeys()
+        
     }
 
     update () {
+        if(this.cursors.left.isDown) {
+            this.character.body.velocity.x = -gameOptions.characterSpeed
+            //this.character.anims.play("left", true)
+        }
+        else if(this.cursors.right.isDown) {
+            this.character.body.velocity.x = gameOptions.characterSpeed
+            //this.character.anims.play("right", true)
+        }
+        else {
+            this.character.body.velocity.x = 0
+            //this.character.anims.play("turn", true)
+        }
+
+    }
+
+
+    loadLevel1() {
+        this.background = this.add.image(0,0,"bg").setOrigin(0,0);
+        this.background.displayWidth = gameOptions.windowWidth;
+        this.background.displayHeight = gameOptions.windowHeight;
+        
+        this.groundGroup = this.physics.add.group({
+            immovable: true,
+            allowGravity: false
+        })
+
+        this.groundGroup.create(32,64,"sideBlock").setOrigin(0,0)
+        for (let index = 0; index < gameOptions.windowWidth/gameOptions.widthOfTile; index++) {
+            this.groundGroup.create((index*gameOptions.widthOfTile),(gameOptions.windowHeight-gameOptions.widthOfTile),("innerBlock")).setOrigin(0,0);
+            
+        }
+
+
 
     }
 }
+
