9b403c399b377362cdfa593da5c93c642ca0118c
Roni Seppälä <roni.o.seppala@gmail.com>
2023-10-26 15:06:55 +0300

    did animations

--- js/script.js
+++ js/script.js
@@ -44,6 +44,9 @@ class PlayGame extends Phaser.Scene {
         this.load.image("sideBlock","assets/stageObjects/Tiles/IndustrialTile_05.png")
         this.load.image("innerBlock", "assets/stageObjects/Tiles/IndustrialTile_21.png")
         this.load.spritesheet("characterIdle", "assets/character/Idle.png",{frameWidth: 72, frameHeight: 72})
+        this.load.spritesheet("characterWalk","assets/character/Walk.png",{frameWidth: 72, frameHeight: 72})
+        this.load.spritesheet("characterJump","assets/character/Attack4.png",{frameWidth: 72, frameHeight: 72})
+
     }
 
     create () {
@@ -57,28 +60,65 @@ class PlayGame extends Phaser.Scene {
         this.character.hasHeldJumpFor = 0;
         this.character.hasTouchedGroundFor = 0;
         this.character.doubleJumpTimer = 0;
+        this.character.jumpAnimationCounter = 0;
+        this.character.jumpAnimationTimeCounter = 0;
         this.cursors = this.input.keyboard.createCursorKeys()
+
+        this.anims.create({
+            key: "walk",
+            frames: this.anims.generateFrameNumbers("characterWalk", {start: 0, end: 5}),
+            frameRate: 10,
+            repeat: 1
+        })
+
+        this.anims.create({
+            key: "idle",
+            frames: this.anims.generateFrameNumbers("characterIdle", {start: 0, end: 3}),
+            frameRate: 10,
+            repeat: 1
+        })
+
+        this.anims.create({
+            key: "jump",
+            frames: this.anims.generateFrameNumbers("characterJump", {start: 1, end: 2}),
+            frameRate: 2,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: "inAir",
+            frames: this.anims.generateFrameNumbers("characterJump", {start: 2, end: 2}),
+            frameRate: 1,
+            repeat: 1
+        })
+
+        this.anims.create({
+            key: "landing",
+            frames: this.anims.generateFrameNumbers("characterJump", {start: 3, end: 3}),
+            frameRate: 1,
+            repeat: 1
+        })
         
     }
 
-    update () {
+    update () {    
         if(this.cursors.left.isDown) {
+            this.character.setFlipX(true)
             this.character.body.velocity.x = -gameOptions.characterSpeed
-            //this.character.anims.play("left", true)
         }
         else if(this.cursors.right.isDown) {
+            this.character.setFlipX(false)
             this.character.body.velocity.x = gameOptions.characterSpeed
-            //this.character.anims.play("right", true)
         }
         else {
             this.character.body.velocity.x = 0
-            //this.character.anims.play("turn", true)
         }
 
         if(this.character.hasHeldJumpFor > 1 && this.character.hasTouchedGroundFor > 4 && this.character.jumpcount == 1) { //normal jump
             this.character.body.velocity.y = -gameOptions.characterGravity / 1.6
             this.character.jumpcount = 0;
             this.character.hasTouchedGroundFor = 0;
+            this.character.jumpAnimationCounter = 1;
         }else if(this.cursors.up.isDown && this.character.jumpcount == 0 && this.character.hasReleasedJumpFor > 1) { //initiate jetpack in air
             //this.character.body.velocity.y = -gameOptions.characterGravity / 1.6
             this.character.jumpcount = this.character.jumpcount - 1
@@ -87,6 +127,26 @@ class PlayGame extends Phaser.Scene {
         }else if(this.character.hasTouchedGroundFor > 4) {
             this.character.jumpcount = 1
             this.character.doubleJumpTimer = 0
+            this.character.jumpAnimationCounter = 0;
+        }
+
+        //animation handling
+        if (this.character.jumpcount == 1){
+            if(this.cursors.left.isDown) {
+                this.character.anims.play("walk", true)
+            }
+            else if(this.cursors.right.isDown) {
+                this.character.anims.play("walk", true)
+            }
+            else {
+                this.character.anims.play("idle",true)
+            }
+        } else if(this.character.jumpAnimationCounter == 1){
+            this.character.anims.play("jump",true)
+        } else if(this.character.jumpAnimationCounter == 2){
+            this.character.anims.play("inAir",true)
+        } else if (this.character.jumpAnimationCounter == 3){
+            this.character.anims.play("landing",true)
         }
 
         if (this.cursors.up.isDown){//tracker for how long has up been held
@@ -110,7 +170,18 @@ class PlayGame extends Phaser.Scene {
         if (this.character.jumpcount == -1){ //tracker for jetpack frames
             this.character.doubleJumpTimer++
         }
-        
+
+        if (this.character.jumpAnimationCounter == 1 && this.character.jumpAnimationTimeCounter < 10){ //jump animation counter logic
+            this.character.jumpAnimationTimeCounter++
+        } else if(this.character.jumpAnimationCounter == 1 && this.character.jumpAnimationTimeCounter >= 10){
+            this.character.jumpAnimationTimeCounter = 0
+            this.character.jumpAnimationCounter = 2
+        } else if(this.character.jumpAnimationCounter == 2 && this.character.doubleJumpTimer + 50 >= gameOptions.doubleJumpFrames){
+            this.character.jumpAnimationCounter = 3
+            this.character.jumpAnimationTimeCounter = 0
+        }
+
+
 
     }
 
